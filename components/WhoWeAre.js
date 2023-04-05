0ca62723a0755bbde3dd09940d009a493e6662f1
Phemel0 <34798947+Phemel0@users.noreply.github.com>
2023-04-05 09:43:45 +0200

    who we are description tag changed

--- components/WhoWeAre.js
+++ components/WhoWeAre.js
@@ -11,7 +11,7 @@ function WhoWeAre() {
         </div>
         <div className="my-28">
             <div className="text-sm md:text-xl">(TAIMOOR SHAHZADA)</div>
-            <div className="text-3xl md:text-5xl font-bold leading-snug indent-[30vw] my-2">AN INDEPENDENT MUSIC AGENCY HELPING BRANDS TELL POWERFUL, AUTHENTIC
+            <div className="text-3xl md:text-5xl font-bold leading-snug indent-[30vw] my-2">AN INDEPENDENT FRONT-END DEVELOPER HELPING BRANDS TELL POWERFUL, AUTHENTIC
           STORIES WITH MUSIC. WORKING CLOSELY WITH OUR CLIENTS AND OUR CURATED
           GLOBAL NETWORK OF ARTISTS, WE STRATEGISE, PRODUCE, FIND AND LICENSE
           THE BEST MUSIC.</div>
